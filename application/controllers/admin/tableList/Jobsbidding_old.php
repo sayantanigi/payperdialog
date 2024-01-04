217aa95fd4bd57762a2403ad23d64efb6744bd48
sayantanigi <sayantan@goigi.in>
2024-01-04 12:22:54 +0530

    update code

--- application/controllers/admin/tableList/Jobsbidding_old.php
+++ application/controllers/admin/tableList/Jobsbidding_old.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Jobsbidding extends MY_Controller {
+class Jobsbidding extends MY_Controller
+{
 
     public function __construct()
     {
@@ -10,58 +11,59 @@ class Jobsbidding extends MY_Controller {
     }
 
     function index()
-  	{
+    {
 
-  		$header = array('title' => 'jobsBid');
-  		$data = array(
-              'heading' => 'Jobs Bid List',
-          );
-          $this->load->view('admin/header', $header);
-          $this->load->view('admin/sidebar');
-          $this->load->view('admin/table_list/jobbid_list',$data);
-          $this->load->view('admin/footer');
-  	}
+        $header = array('title' => 'jobsBid');
+        $data = array(
+            'heading' => 'Jobs Bid List',
+        );
+        $this->load->view('admin/header', $header);
+        $this->load->view('admin/sidebar');
+        $this->load->view('admin/table_list/jobbid_list', $data);
+        $this->load->view('admin/footer');
+    }
 
-  function ajax_manage_page()
-  	{
-  		 $GetData = $this->Jobsbid_model->get_datatables();
-          if(empty($_POST['start']))
-     		{
+    function ajax_manage_page()
+    {
+        $GetData = $this->Jobsbid_model->get_datatables();
+        if (empty($_POST['start'])) {
 
-      		$no=0;
-         	}
-          else{
-              $no =$_POST['start'];
-          }
-          $data = array();
-          foreach ($GetData as $row)
-          {
+            $no = 0;
+        } else {
+            $no = $_POST['start'];
+        }
+        $data = array();
+        foreach ($GetData as $row) {
 
-               $btn = ''.anchor(admin_url('users/view/'.base64_encode($row->userId)),'<span class="btn btn-sm bg-success-light mr-2"><i class="far fa-eye mr-1"></i>view</span>');
-             // $btn .= ' | '.'<span data-placement="right" class="btn btn-sm btn-danger mr-2"  onclick="Delete(this,'.$row->userId.')">Delete</span>';
+            $btn = '' . anchor(admin_url('users/view/' . base64_encode($row->userId)), '<span class="btn btn-sm bg-success-light mr-2"><i class="far fa-eye mr-1"></i>view</span>');
+            // $btn .= ' | '.'<span data-placement="right" class="btn btn-sm btn-danger mr-2"  onclick="Delete(this,'.$row->userId.')">Delete</span>';
+            if (!empty($row->fullname)) {
+                $name = ucfirst($row->fullname);
+            } else {
+                $name = ucfirst($row->username);
+            }
+            $no++;
+            $nestedData = array();
+            $nestedData[] = $no;
+            $nestedData[] = $name;
+            $nestedData[] = ucfirst($row->post_title);
+            $nestedData[] = $row->duration;
+            $nestedData[] = 'USD' . ' ' . $row->cost;
+            $nestedData[] = date('d-M-Y', strtotime($row->created_date));
+            $nestedData[] = $btn;
+            $data[] = $nestedData;
+        }
 
-  	            $no++;
-  	            $nestedData = array();
-  	          $nestedData[] = $no;
-  	            $nestedData[] = if(!empty($row->fullname)){ echo ucfirst($row->fullname);} else{ echo $row->username};
-                $nestedData[] = ucfirst($row->post_title);
-                $nestedData[] = $row->duration;
-                $nestedData[] = 'USD'.' '.$row->cost;
-                $nestedData[] = date('d-M-Y',strtotime($row->created_date));
-  	            $nestedData[] = $btn;
-  	            $data[] = $nestedData;
-          }
+        $output = array(
+            "draw" => $_POST['draw'],
+            "recordsTotal" => $this->Jobsbid_model->count_all(),
+            "recordsFiltered" => $this->Jobsbid_model->count_filtered(),
+            "data" => $data,
+        );
 
-      	$output = array(
-                  "draw" => $_POST['draw'],
-                  "recordsTotal" => $this->Jobsbid_model->count_all(),
-                  "recordsFiltered" => $this->Jobsbid_model->count_filtered(),
-                  "data" => $data,
-          );
-
-      	echo json_encode($output);
-  	}
+        echo json_encode($output);
+    }
 
 
 
-}//end controller
+} //end controller
