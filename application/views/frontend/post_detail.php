906129727771f3839270d1491c4e54d2d7f5a383
care@goigi.com <sayantan@goigi.in>
2025-01-16 17:14:57 +0530

    code update

--- application/views/frontend/post_detail.php
+++ application/views/frontend/post_detail.php
@@ -267,7 +267,7 @@ if (!empty($get_banner->image) && file_exists('uploads/banner/' . $get_banner->i
             <div class="row">
                 <div class="col-lg-12">
                     <div class="row no-gape">
-                        <h3>Recomended Employee</h3>
+                        <h3>Recommended Employee</h3>
                     </div>
                 </div>
             </div>
@@ -283,20 +283,39 @@ if (!empty($get_banner->image) && file_exists('uploads/banner/' . $get_banner->i
                             $key_skills = explode(',', $post_data->required_key_skills);
                             $experience = $post_data->experience_level;
                             for ($i=0; $i < count($key_skills); $i++) {
-                                $recomendedEmployeeList = $this->db->query("SELECT * FROM users WHERE (skills = '".trim($key_skills[$i])."' OR skills LIkE '%".trim($key_skills[$i])."%') AND experience = '".$experience."'")->result_array();
+                                //$recomendedEmployeeList = $this->db->query("SELECT * FROM users WHERE (skills = '".trim($key_skills[$i])."' OR skills LIKE '%".trim($key_skills[$i])."%') AND experience <= '".$experience."'")->result_array();
+                                $recomendedEmployeeList = $this->db->query("SELECT * FROM `users` WHERE instr(concat(',', skills, ','), ',$key_skills[$i],') AND experience <= '".$experience."' AND `status` = 1 AND `email_verified` = 1 AND userType = '1' ORDER BY experience DESC")->result_array();
                                 foreach ($recomendedEmployeeList as $value) {
-                                    $profile_pic= '<img src="'.base_url('uploads/users/'.$value['profilePic']).'" alt="" />';
+                                    if(!empty($value['profilePic']) && file_exists('uploads/users/'.$value['profilePic'])) {
+                                        $profile_pic= '<img src="'.base_url('uploads/users/'.$value['profilePic']).'" alt="" />';
+                                    } else {
+                                        $profile_pic= '<img src="'.base_url('uploads/no_user.png').'" alt="" />';
+                                    }
+
                                     if(strlen($value['short_bio'])>100) {
                                         $desc= substr(strip_tags($value['short_bio']), 0,100).'...';
                                     } else {
                                         $desc= strip_tags($value['short_bio']);
                                     }
                                 ?>
-                                <div class="emply-resume-list"> <div class="emply-resume-thumb"><?= $profile_pic ?></div> <div class="emply-resume-info"> <h3><a href="#" title=""><?= $value['firstname]']." ".$value['lastname']?></a></h3><p><i class="la la-map-marker"></i><?= $value['address']?></p> <p><?= $value['address']?><?= $desc?></p> <p></p> </div> <div class="shortlists" style="width:50px;"><a href="<?= base_url('employerdetail/'.base64_encode($value['userId']))?>" title="">View Profile<i class="la la-plus"></i></a> </div> </div>
+                                <div class="emply-resume-list">
+                                    <div class="emply-resume-thumb"><?= $profile_pic ?></div>
+                                    <div class="emply-resume-info">
+                                        <h3>
+                                            <a href="<?= base_url('employerdetail/'.base64_encode($value['userId']))?>" title=""><?= $value['firstname']." ".$value['lastname']?></a>
+                                        </h3>
+                                        <p><i class="la la-map-marker"></i><?= $value['address']?></p>
+                                        <p><?= $value['address']?><?= $desc?></p>
+                                        <p></p>
+                                    </div>
+                                    <div class="shortlists" style="width:50px;">
+                                        <a href="<?= base_url('employerdetail/'.base64_encode($value['userId']))?>" title="">View Profile<i class="la la-plus"></i></a>
+                                    </div>
+                                </div>
                                 <?php }
                             } ?>
                             </div>
-                            <div align="center" id="pagination_link"></div>
+                            <div align="center" id="pagination_link">Unfortunately, no employee has been identified with the specific skill set required for this job posting at this time.</div>
                         </div>
                     </div>
                 </div>
